4dac4be921bbef10bf55e88cdda650f26adf4e2a
Neil Roberts <bpeeluk@yahoo.co.uk>
2020-10-28 18:23:07 +0100

    Initial code

---
+++ cma-pool-log.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3
+
+import re
+import sys
+
+
+class Buffer:
+    def __init__(self, size, unmoveable=False):
+        self.size = size
+        self.unmoveable = unmoveable
+        self.offset = None
+        self.in_use = False
+
+
+class Pool:
+    LINE_RE = re.compile(r'^\[ *([0-9]+)\.([0-9]{6}) *] +@@@ +([a-z_]+) '
+                         r'([0-9a-f]{8,16})(?: +(.*))?')
+    SIZE = 128 * 1024 * 1024
+
+    def __init__(self, compact=False):
+        self.buffers = {}
+        self.mru_list = []
+        self.offset_list = []
+        self.compact = compact
+
+    def process_line(self, line):
+        md = Pool.LINE_RE.match(line)
+        if md is None:
+            return
+
+        timestamp = int(md.group(1)) * 1_000_000 + int(md.group(2))
+        cmd = md.group(3)
+        buf_id = int(md.group(4), 16)
+        args = md.group(5)
+
+        Pool.COMMANDS[cmd](self, timestamp, buf_id, args)
+
+    def _page_out_lru_buffer(self):
+        for pos in range(len(self.mru_list) - 1, -1, -1):
+            buf = self.mru_list[pos]
+
+            if buf.in_use or buf.unmoveable:
+                continue
+
+            self.offset_list.remove(buf)
+            del self.mru_list[pos]
+            buf.offset = None
+
+            return True
+
+        return False
+
+    def _insert_buffer(self, buf):
+        last_offset = 0
+
+        for pos, other_buf in enumerate(self.offset_list):
+            if other_buf.offset - last_offset >= buf.size:
+                buf.offset = last_offset
+                self.offset_list.insert(pos, buf)
+                self.mru_list.insert(0, buf)
+                return True
+
+            last_offset = other_buf.offset + buf.size
+
+        if last_offset + buf.size <= Pool.SIZE:
+            buf.offset = last_offset
+            self.offset_list.append(buf)
+            self.mru_list.insert(0, buf)
+            return True
+
+        return False
+
+    def _page_in_buffer(self, buf):
+        if buf.offset is not None:
+            return
+
+        while not self._insert_buffer(buf):
+            if not self._page_out_lru_buffer():
+                raise Exception("Couldn’t make space for buffer of size {}".
+                                format(buf.size))
+
+    def _compact_buffer(self, buf):
+        pass
+
+    def _buf_destroy(self, timestamp, buf_id, args):
+        assert(args is None)
+
+        buf = self.buffers[buf_id]
+
+        if buf.offset is not None:
+            self.mru_list.remove(buf)
+            self.offset_list.remove(buf)
+
+        del self.buffers[buf_id]
+
+    def _buf_add_usecnt(self, timestamp, buf_id, args):
+        assert(args is None)
+
+        buf = self.buffers[buf_id]
+        assert(not buf.in_use)
+        buf.in_use = True
+
+    def _buf_remove_usecnt(self, timestamp, buf_id, args):
+        assert(args is None)
+
+        buf = self.buffers[buf_id]
+        assert(buf.in_use)
+        buf.in_use = False
+
+        if self.compact:
+            self._compact_buffer(buf)
+
+    def _buf_create(self, timestamp, buf_id, args):
+        assert(buf_id not in self.buffers)
+
+        arg_parts = args.split(' ')
+        buf = Buffer(int(arg_parts[0]), len(arg_parts) > 1)
+        self.buffers[buf_id] = buf
+
+        self._page_in_buffer(buf)
+
+    def _buf_use(self, timestamp, buf_id, args):
+        assert(args is None)
+
+        buf = self.buffers[buf_id]
+        self._page_in_buffer(buf)
+
+        assert(buf.offset is not None)
+
+        self.mru_list.remove(buf)
+        self.mru_list.insert(0, buf)
+
+    COMMANDS = {
+        'destroy' : _buf_destroy,
+        'add_usecnt' : _buf_add_usecnt,
+        'remove_usecnt' : _buf_remove_usecnt,
+        'create' : _buf_create,
+        'use' : _buf_use,
+    }
+
+
+def main():
+    pool = Pool()
+
+    for line in sys.stdin:
+        pool.process_line(line)
+
+
+if __name__ == '__main__':
+    main()
